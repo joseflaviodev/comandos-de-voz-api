8a5c5c4e9773f3530c29486263a64d0cbbb463ca
José Flávio <jose.flavio@adaptweb.com.br>
2024-10-16 16:08:21 -0300

    cors

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pickle
 from fastapi import FastAPI
-from sklearn.feature_extraction.text import CountVectorizer
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Accept requests from any origin
+    allow_credentials=True,  # Whether to allow cookies
+    allow_methods=["*"],     # Allow all HTTP methods
+    allow_headers=["*"],     # Allow all headers
+)
 
 with open('modelos/classificador_naive_bayes.pkl', 'rb') as file:
     classificador = pickle.load(file)
